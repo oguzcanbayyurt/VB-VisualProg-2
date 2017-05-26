9b33590c59728eefd0099a35c842be8029f2d56c
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-26 10:18:34 +0300

    Initial

---
+++ Access - DataGridView/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(205, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(655, 225)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Tüm Filmler", "Polisiye", "Aksiyon"})
+        Me.ComboBox1.Location = New System.Drawing.Point(59, 45)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(897, 404)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Access - DataGridView/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class Form1
+    Dim baglanti As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=veri.accdb")
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim komut As New OleDbDataAdapter("select * from kayit", baglanti)
+        Dim ds As New DataSet
+        komut.Fill(ds)
+        DataGridView1.DataSource = ds.Tables(0)
+        DataGridView1.Columns(0).HeaderText = "Film Adı"
+        DataGridView1.Columns(1).HeaderText = "Film Konusu"
+        DataGridView1.Columns(2).HeaderText = "Film Türü"
+        DataGridView1.Columns(3).HeaderText = "Yapım Yılı"
+        DataGridView1.Columns(4).HeaderText = "IMDB Puanı"
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "Tüm Filmler" Then
+            Dim komut As New OleDbDataAdapter("select * from kayit", baglanti)
+            Dim ds As New DataSet
+            komut.Fill(ds)
+            DataGridView1.DataSource = ds.Tables(0)
+        Else
+            Dim komut As New OleDbDataAdapter("select * from kayit where tur='" & ComboBox1.Text & "'", baglanti)
+            Dim ds As New DataSet
+            komut.Fill(ds)
+            DataGridView1.DataSource = ds.Tables(0)
+        End If
+    End Sub
+End Class
