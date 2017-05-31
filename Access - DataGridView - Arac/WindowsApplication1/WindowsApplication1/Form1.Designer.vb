69352c32db3444bf5cd8d1454cb06706cf4cd424
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-31 12:04:53 +0300

    Initial

---
+++ Access - DataGridView - Arac/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SilToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 125)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Marka:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(49, 90)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Yıl:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 50)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Plaka:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 161)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 24)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Renk:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.ContextMenuStrip = Me.ContextMenuStrip1
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.MenuText
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Red
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.Location = New System.Drawing.Point(237, 48)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(457, 225)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(98, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(120, 29)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label5.Location = New System.Drawing.Point(453, 22)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(115, 16)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Plakaya Göre Ara"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(575, 13)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(119, 29)
+        Me.TextBox2.TabIndex = 7
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"2012", "2013", "2014", "2015", "2016"})
+        Me.ComboBox1.Location = New System.Drawing.Point(97, 87)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 32)
+        Me.ComboBox1.TabIndex = 8
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Audi", "BMW", "Chavrolet", "Citroen", "Dacia", "Ford", "Honda", "Mercedes", "Wolksvagen"})
+        Me.ComboBox2.Location = New System.Drawing.Point(97, 126)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 32)
+        Me.ComboBox2.TabIndex = 9
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Beyaz", "Gri", "Kırmızı", "Siyah", "Mavi"})
+        Me.ComboBox3.Location = New System.Drawing.Point(97, 163)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 32)
+        Me.ComboBox3.TabIndex = 10
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button1.Location = New System.Drawing.Point(43, 216)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 31)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Kaydet"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button2.Location = New System.Drawing.Point(142, 216)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 31)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Güncelle"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SilToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(85, 26)
+        '
+        'SilToolStripMenuItem
+        '
+        Me.SilToolStripMenuItem.Name = "SilToolStripMenuItem"
+        Me.SilToolStripMenuItem.Size = New System.Drawing.Size(84, 22)
+        Me.SilToolStripMenuItem.Text = "Sil"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 343)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents SilToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Access - DataGridView - Arac/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class Form1
+    Dim baglanti As New OleDbConnection("provider= Microsoft.ace.oledb.12.0; Data source= veri.accdb")
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim komut As New OleDbCommand("select * from kayit where plaka= '" & TextBox1.Text & "'", baglanti)
+        Dim dr As OleDbDataReader
+        baglanti.Open()
+        dr = komut.ExecuteReader
+        If dr.HasRows Then
+            MsgBox("Bu kullanıcı adı sisteme kayıtlı")
+            baglanti.Close()
+        Else
+            baglanti.Close()
+
+            baglanti.Open()
+            komut.CommandText = " insert into kayit(marka,model,plaka,renk) values ('" & ComboBox2.Text & "','" & ComboBox1.Text & "','" & TextBox1.Text & "','" & ComboBox3.Text & "') "
+            komut.ExecuteNonQuery()
+            MsgBox("Kayıt Başarılı")
+            baglanti.Close()
+            listele()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        listele()
+    End Sub
+    Private Sub listele()
+        Dim komut As New OleDbDataAdapter("select * from kayit", baglanti)
+        Dim ds As New DataSet
+        komut.Fill(ds)
+        DataGridView1.DataSource = ds.Tables(0)
+        baglanti.Close()
+        DataGridView1.Columns(0).HeaderText = "PLAKA"
+        DataGridView1.Columns(1).HeaderText = "YIL"
+        DataGridView1.Columns(2).HeaderText = "MARKA"
+        DataGridView1.Columns(3).HeaderText = "RENK"
+    End Sub
+    Private Sub TextBox2_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+        Dim komut As New OleDbDataAdapter("select * from kayit where plaka like '%" & TextBox2.Text & "%'  ", baglanti)
+        Dim ds As New DataSet
+        komut.Fill(ds)
+        DataGridView1.DataSource = ds.Tables(0)
+        DataGridView1.Columns(0).HeaderText = "PLAKA"
+        DataGridView1.Columns(1).HeaderText = "YIL"
+        DataGridView1.Columns(2).HeaderText = "MARKA"
+        DataGridView1.Columns(3).HeaderText = "RENK"
+    End Sub
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim komut As New OleDbCommand("update kayit set model='" & ComboBox1.Text & "', marka='" & ComboBox2.Text & "', renk='" & ComboBox3.Text & "' where plaka= '" & TextBox1.Text & "' ", baglanti)
+        baglanti.Open()
+        komut.ExecuteNonQuery()
+        baglanti.Close()
+        listele()
+    End Sub
+    Private Sub DataGridView1_DoubleClick(sender As Object, e As System.EventArgs) Handles DataGridView1.DoubleClick
+        ComboBox1.Text = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("model").Value
+        ComboBox2.Text = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("marka").Value
+        TextBox1.Text = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("plaka").Value
+        ComboBox3.Text = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("renk").Value
+    End Sub
+
+    Private Sub SilToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles SilToolStripMenuItem.Click
+        Dim plaka As String = DataGridView1.Rows(DataGridView1.CurrentCell.RowIndex).Cells("plaka").Value
+        Dim komut As New OleDbCommand("delete from kayit where plaka= '" & plaka & "' ", baglanti)
+        baglanti.Open()
+        komut.ExecuteNonQuery()
+        baglanti.Close()
+        listele()
+    End Sub
+End Class
