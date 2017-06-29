6b465532701bded4305cb70c6fe6b4bdd18a1586
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-06-29 11:41:10 +0300

    Initial

---
+++ Basit Sinema Koltuk Secimi/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(57, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(82, 36)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(177, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(82, 36)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(57, 81)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(82, 36)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(177, 82)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(82, 36)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(57, 124)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(82, 36)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(177, 125)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(82, 36)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button6)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(52, 73)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(317, 224)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "CİNSİYET"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"KADIN", "ERKEK"})
+        Me.ComboBox1.Location = New System.Drawing.Point(104, 11)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 8
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Blue
+        Me.Button7.ForeColor = System.Drawing.Color.White
+        Me.Button7.Location = New System.Drawing.Point(391, 141)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(109, 50)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "Bilet Al"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(577, 396)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+
+End Class
---
+++ Basit Sinema Koltuk Secimi/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+    Dim koltuk1, koltuk2, koltuk3, koltuk4, koltuk5, koltuk6 As Integer
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        koltuk1 = koltuk1 + 1
+        If koltuk1 Mod 2 = 1 And ComboBox1.Text = "KADIN" Then
+            Button1.BackColor = Color.Red
+            Button1.ForeColor = Color.White
+        ElseIf koltuk1 Mod 2 = 1 And ComboBox1.Text = "ERKEK" Then
+            Button1.BackColor = Color.Blue
+            Button1.ForeColor = Color.White
+
+        Else
+            Button1.BackColor = Color.Transparent
+            Button1.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        koltuk2 = koltuk2 + 1
+        If koltuk2 Mod 2 = 1 And ComboBox1.Text = "KADIN" Then
+            Button2.BackColor = Color.Red
+            Button2.ForeColor = Color.White
+        ElseIf koltuk1 Mod 2 = 1 And ComboBox1.Text = "ERKEK" Then
+            Button2.BackColor = Color.Blue
+            Button2.ForeColor = Color.White
+        Else
+            Button2.BackColor = Color.Transparent
+            Button2.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        koltuk3 = koltuk3 + 1
+        If koltuk3 Mod 2 = 1 And ComboBox1.Text = "KADIN" Then
+            Button3.BackColor = Color.Red
+            Button3.ForeColor = Color.White
+        ElseIf koltuk1 Mod 2 = 1 And ComboBox1.Text = "ERKEK" Then
+            Button3.BackColor = Color.Blue
+            Button3.ForeColor = Color.White
+        Else
+            Button3.BackColor = Color.Transparent
+            Button3.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        koltuk4 = koltuk4 + 1
+        If koltuk4 Mod 2 = 1 And ComboBox1.Text = "KADIN" Then
+            Button4.BackColor = Color.Red
+            Button4.ForeColor = Color.White
+        ElseIf koltuk1 Mod 2 = 1 And ComboBox1.Text = "ERKEK" Then
+            Button4.BackColor = Color.Blue
+            Button4.ForeColor = Color.White
+        Else
+            Button4.BackColor = Color.Transparent
+            Button4.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        koltuk5 = koltuk5 + 1
+        If koltuk5 Mod 2 = 1 And ComboBox1.Text = "KADIN" Then
+            Button5.BackColor = Color.Red
+            Button5.ForeColor = Color.White
+        ElseIf koltuk1 Mod 2 = 1 And ComboBox1.Text = "ERKEK" Then
+            Button5.BackColor = Color.Blue
+            Button5.ForeColor = Color.White
+        Else
+            Button5.BackColor = Color.Transparent
+            Button5.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        koltuk6 = koltuk6 + 1
+        If koltuk6 Mod 2 = 1 And ComboBox1.Text = "KADIN" Then
+            Button6.BackColor = Color.Red
+            Button6.ForeColor = Color.White
+        ElseIf koltuk1 Mod 2 = 1 And ComboBox1.Text = "ERKEK" Then
+            Button6.BackColor = Color.Blue
+            Button6.ForeColor = Color.White
+        Else
+            Button6.BackColor = Color.Transparent
+            Button6.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.Text = "ERKEK" Or ComboBox1.Text = "KADIN" Then
+            GroupBox1.Enabled = True
+        Else
+            GroupBox1.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Button7.Click
+        Dim butonlar(6) As Button
+        butonlar(1) = Button1
+        butonlar(2) = Button2
+        butonlar(3) = Button3
+        butonlar(4) = Button4
+        butonlar(5) = Button5
+        butonlar(6) = Button6
+        For i = 1 To 6
+            If butonlar(i).BackColor = Color.Red Or butonlar(i).BackColor = Color.Blue Then
+                butonlar(i).Enabled = False
+                butonlar(i).Text = "DOLU"
+            End If
+        Next
+    End Sub
+End Class
