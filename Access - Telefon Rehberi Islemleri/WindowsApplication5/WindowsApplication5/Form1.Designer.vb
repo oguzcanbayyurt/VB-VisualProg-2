a4f325e28e9fb57cd3e459674af2296994ddb551
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-06-29 11:38:50 +0300

    Initial

---
+++ Access - Telefon Rehberi Islemleri/WindowsApplication5/WindowsApplication5/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(96, 182)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "KAYDET"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.TextBox1.Location = New System.Drawing.Point(121, 56)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(134, 26)
+        Me.TextBox1.TabIndex = 1
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(121, 98)
+        Me.MaskedTextBox1.Margin = New System.Windows.Forms.Padding(5)
+        Me.MaskedTextBox1.Mask = "(999) 000-0000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(134, 26)
+        Me.MaskedTextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(11, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "AD-SOYAD"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(24, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "TELEFON"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"AİLE", "ARKADAŞ", "ÜNİVERSİTE", "SOHBET"})
+        Me.ComboBox1.Location = New System.Drawing.Point(121, 137)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(134, 28)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(50, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "GRUP"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(117, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(175, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "TELEFON REHBERİ"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button2.Location = New System.Drawing.Point(263, 53)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(53, 26)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "BUL"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button3.Location = New System.Drawing.Point(263, 98)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(53, 26)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "BUL"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button4.Location = New System.Drawing.Point(338, 72)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 29)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "Güncelle"
+        Me.Button4.UseVisualStyleBackColor = False
+        Me.Button4.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Button5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button5.Location = New System.Drawing.Point(207, 182)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(85, 35)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Temizle"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button6.Location = New System.Drawing.Point(158, 235)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 34)
+        Me.Button6.TabIndex = 11
+        Me.Button6.Text = "SİL"
+        Me.Button6.UseVisualStyleBackColor = False
+        Me.Button6.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(549, 363)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents MaskedTextBox1 As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ Access - Telefon Rehberi Islemleri/WindowsApplication5/WindowsApplication5/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class Form1
+    Dim baglanti As New OleDbConnection("Provider= Microsoft.Ace.Oledb.12.0; Data source=veri.accdb")
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim komut As New OleDbCommand("select * from kayit where ad_soyad= '" & TextBox1.Text & "'", baglanti)
+        Dim dr As OleDbDataReader
+        baglanti.Open()
+        dr = komut.ExecuteReader
+        If dr.HasRows Then
+            Do While dr.Read
+                MaskedTextBox1.Text = dr("tel")
+                ComboBox1.Text = dr("grup")
+                Button4.Visible = True
+                MaskedTextBox1.Enabled = False
+                Button6.Visible = True
+            Loop
+        Else
+            MsgBox(" Kayıt bulunamadı")
+        End If
+        baglanti.Close()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Dim komut As New OleDbCommand("select * from kayit where tel= '" & MaskedTextBox1.Text & "'", baglanti)
+        Dim dr As OleDbDataReader
+        baglanti.Open()
+        dr = komut.ExecuteReader
+        If dr.HasRows Then
+            Do While dr.Read
+                TextBox1.Text = dr("ad_soyad")
+                ComboBox1.Text = dr("grup")
+                Button4.Visible = True
+                MaskedTextBox1.Enabled = False
+                Button6.Visible = True
+            Loop
+        Else
+            MsgBox("Kayıtlı kişi bulunamadı")
+        End If
+        baglanti.Close()
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        temizle()
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Dim komut As New OleDbCommand("update kayit set ad_soyad='" & TextBox1.Text & "', grup='" & ComboBox1.Text & "' where tel='" & MaskedTextBox1.Text & "'", baglanti)
+        baglanti.Open()
+        komut.ExecuteNonQuery()
+        baglanti.Close()
+        MsgBox("Güncelleme Başarılı")
+
+    End Sub
+    Private Sub temizle()
+        TextBox1.Clear()
+        MaskedTextBox1.Clear()
+        ComboBox1.Text = ""
+        Button4.Visible = False
+        MaskedTextBox1.Enabled = True
+        Button6.Visible = False
+    End Sub
+  
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        Dim komut As New OleDbCommand("delete from kayit  where tel='" & MaskedTextBox1.Text & "'", baglanti)
+        baglanti.Open()
+        komut.ExecuteNonQuery()
+        baglanti.Close()
+        MsgBox("Silme İşlemi Başarılı")
+        temizle()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or MaskedTextBox1.Text = "(   )    -" Then
+            MsgBox(" Boş alanları doldurunuz")
+        Else
+            Dim komut As New OleDbCommand(" select * from kayit where tel= '" & MaskedTextBox1.Text & "'", baglanti)
+            Dim dr As OleDbDataReader
+            baglanti.Open()
+            dr = komut.ExecuteReader
+            If dr.HasRows Then
+                MsgBox("Bu numara kayıtlı")
+                baglanti.Close()
+            Else
+                baglanti.Close()
+                komut.CommandText = "insert into kayit(ad_soyad,tel,grup) values( '" & TextBox1.Text & "','" & MaskedTextBox1.Text & "','" & ComboBox1.Text & "')"
+                baglanti.Open()
+                komut.ExecuteNonQuery()
+                baglanti.Close()
+                MsgBox("Kayıt başarılı")
+            End If
+        End If
+    End Sub
+End Class
